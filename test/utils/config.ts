a29f7679a5df66f3e4bde15080f4c5a74ae272d3
Michał Mach <vipmachu@gmail.com>
2022-03-29 15:12:46 +0200

    new reserves deployment setup

--- test/utils/config.ts
+++ test/utils/config.ts
@@ -3,3 +3,17 @@ export const ethMainnetFeedRegistryAddress =
 export const ethAddress = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE";
 export const wethAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
 export const wbtcAddress = "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599";
+export const daiAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+export const uniRouterAddress = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
+
+export const erc20Abi = [
+  "function allowance(address owner, address spender) external view returns (uint256 remaining)",
+  "function approve(address spender, uint256 value) external returns (bool success)",
+  "function balanceOf(address owner) external view returns (uint256 balance)",
+  "function decimals() external view returns (uint8 decimalPlaces)",
+  "function name() external view returns (string memory tokenName)",
+  "function symbol() external view returns (string memory tokenSymbol)",
+  "function totalSupply() external view returns (uint256 totalTokensIssued)",
+  "function transfer(address to, uint256 value) external returns (bool success)",
+  "function transferFrom(address from, address to, uint256 value) external returns (bool success)",
+];
--- test/utils/setup.ts
+++ test/utils/setup.ts
@@ -1,67 +1,94 @@
+/* eslint-disable camelcase */
+/* eslint-disable node/no-missing-import */
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import { expect } from "chai";
 import { BigNumber } from "ethers";
 import { ethers } from "hardhat";
 import { LendingProtocol } from "../../typechain";
-// import { Errors, LendingProtocol } from "../../typechain";
-// import { PriceConsumer } from "../../typechain";
 import {
   ethMainnetFeedRegistryAddress,
   wethAddress,
   ethAddress,
+  daiAddress,
+  uniRouterAddress,
+  erc20Abi,
+  wbtcAddress,
 } from "./config";
 
 export const defaultCollateralFactor = 75;
 export const defaultLiquidationIncentive = 5;
 export const randomValidAddress = ethers.Wallet.createRandom().address;
 export const wethDecimals = 18;
+export const daiDecimals = 18;
+export const wbtcDecimals = 8;
 
 export const deployContracts = async () => {
   const ErrorsFactory = await ethers.getContractFactory("Errors");
   const Errors = await ErrorsFactory.deploy();
 
-  const PriceConsumerFactory = await ethers.getContractFactory("PriceConsumer");
-  const PriceConsumer = await PriceConsumerFactory.deploy(
+  const PriceFeedMockFactory = await ethers.getContractFactory("PriceFeedMock");
+  const PriceFeedMock = await PriceFeedMockFactory.deploy(randomValidAddress);
+
+  const PriceFeedFactory = await ethers.getContractFactory("PriceFeed");
+  const PriceFeed = await PriceFeedFactory.deploy(
     ethMainnetFeedRegistryAddress
   );
-  await PriceConsumer.updateFeedPriceAddress(wethAddress, ethAddress);
+  await PriceFeed.updateAssetPriceFeedAddress(wethAddress, ethAddress);
 
   const LendingProtocolFactory = await ethers.getContractFactory(
     "LendingProtocol"
   );
   const LendingProtocol = await LendingProtocolFactory.deploy(
-    PriceConsumer.address
+    PriceFeed.address
   );
 
   return {
     Errors,
-    PriceConsumer,
+    PriceFeed,
+    PriceFeedMock,
     LendingProtocol,
   };
 };
 
-export const deployWETHRToken = async (lendingProtocolAddress: string) => {
+const deployRToken = async (
+  lendingProtocolAddress: string,
+  asset: string,
+  name: string,
+  symbol: string
+) => {
   const RTokenFactory = await ethers.getContractFactory("RToken");
   const RToken = await RTokenFactory.deploy(
     lendingProtocolAddress,
-    wethAddress,
-    "ReserveWETH",
-    "RWETH"
+    asset,
+    name,
+    symbol
   );
 
   return { RToken };
 };
 
-export const deployWETHDToken = async (lendingProtocolAddress: string) => {
+const deployDToken = async (
+  lendingProtocolAddress: string,
+  asset: string,
+  name: string,
+  symbol: string
+) => {
   const DTokenFactory = await ethers.getContractFactory("DToken");
   const DToken = await DTokenFactory.deploy(
     lendingProtocolAddress,
-    wethAddress,
-    "DebtWETH",
-    "DWETH"
+    asset,
+    name,
+    symbol
   );
 
   return { DToken };
+};
+
+export const deployWETHRToken = async (lendProtocolAddress: string) => {
+  return deployRToken(lendProtocolAddress, wethAddress, "ReserveWETH", "RWETH");
+};
+
+export const deployWETHDToken = async (lendingProtocolAddress: string) => {
+  return deployDToken(lendingProtocolAddress, wethAddress, "DebtWETH", "DWETH");
 };
 
 export const deployWETHRTokenAndInitReserve = async (
@@ -84,21 +111,13 @@ export const deployWETHRTokenAndInitReserve = async (
 };
 
 export const wethContractWithSigner = (signer: SignerWithAddress) => {
-  const wetAbi = [
-    "function allowance(address owner, address spender) external view returns (uint256 remaining)",
-    "function approve(address spender, uint256 value) external returns (bool success)",
-    "function balanceOf(address owner) external view returns (uint256 balance)",
-    "function decimals() external view returns (uint8 decimalPlaces)",
-    "function name() external view returns (string memory tokenName)",
-    "function symbol() external view returns (string memory tokenSymbol)",
-    "function totalSupply() external view returns (uint256 totalTokensIssued)",
-    "function transfer(address to, uint256 value) external returns (bool success)",
-    "function transferFrom(address from, address to, uint256 value) external returns (bool success)",
+  const wethAbi = [
+    ...erc20Abi,
     "function deposit() external payable",
     "function withdraw(uint wad) external",
   ];
 
-  return new ethers.Contract(wethAddress, wetAbi, signer);
+  return new ethers.Contract(wethAddress, wethAbi, signer);
 };
 
 export const swapETHForWETH = async (
@@ -125,4 +144,90 @@ export const swapETHForWETHAndApprove = async (
   await swapETHForWETH(signer, amount);
   const weth = wethContractWithSigner(signer);
   await weth.approve(spender, amount);
+};
+
+export const deployDaiRToken = async (lendingProtocolAddress: string) => {
+  return deployRToken(lendingProtocolAddress, daiAddress, "ReserveDAI", "RDAI");
+};
+
+export const deployDaiDToken = async (lendingProtocolAddress: string) => {
+  return deployDToken(lendingProtocolAddress, daiAddress, "ReserveDAI", "RDAI");
+};
+
+export const deployDaiRTokenAndInitReserve = async (
+  LendingProtocol: LendingProtocol
+) => {
+  const { RToken } = await deployDaiRToken(LendingProtocol.address);
+  const { DToken } = await deployDaiDToken(LendingProtocol.address);
+
+  await LendingProtocol.initReserve(
+    daiAddress,
+    RToken.address,
+    DToken.address,
+    defaultCollateralFactor,
+    defaultLiquidationIncentive,
+    daiDecimals,
+    true
+  );
+
+  return { RToken, DToken };
+};
+
+export const swapETHForDaiAndDeposit = async (
+  signer: SignerWithAddress,
+  amount: BigNumber,
+  LendingProtocol: LendingProtocol
+) => {
+  const uniRouterAbi = [
+    "function swapExactETHForTokens(uint amountOutMin, address[] path, address to, uint deadline) external payable returns (uint[] amounts)",
+  ];
+  const uniRouter = new ethers.Contract(uniRouterAddress, uniRouterAbi, signer);
+
+  const tx = await uniRouter.swapExactETHForTokens(
+    0,
+    [wethAddress, daiAddress],
+    signer.address,
+    ethers.constants.MaxUint256,
+    {
+      value: amount,
+    }
+  );
+  await tx.wait();
+
+  const dai = new ethers.Contract(daiAddress, erc20Abi, signer);
+
+  const daiAmount = await dai.balanceOf(signer.address);
+  console.log("daiAmount", daiAmount);
+
+  await dai.approve(LendingProtocol.address, daiAmount);
+  await LendingProtocol.connect(signer).deposit(daiAddress, daiAmount);
+
+  return daiAmount;
+};
+
+export const deployBtcRToken = async (lendProtocolAddress: string) => {
+  return deployRToken(lendProtocolAddress, wbtcAddress, "ReserveWBTC", "RWBTC");
+};
+
+export const deployBtcDToken = async (lendProtocolAddress: string) => {
+  return deployDToken(lendProtocolAddress, wbtcAddress, "DebtWBTC", "RWBTC");
+};
+
+export const deployBtcRTokenAndInitReserve = async (
+  LendingProtocol: LendingProtocol
+) => {
+  const { RToken } = await deployBtcRToken(LendingProtocol.address);
+  const { DToken } = await deployBtcDToken(LendingProtocol.address);
+
+  await LendingProtocol.initReserve(
+    wbtcAddress,
+    RToken.address,
+    DToken.address,
+    defaultCollateralFactor,
+    defaultLiquidationIncentive,
+    wbtcDecimals,
+    true
+  );
+
+  return { RToken, DToken };
 };
