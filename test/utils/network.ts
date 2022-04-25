1739714cae8324ad87c8279d11c8af213ca35d47
Michał Mach <vipmachu@gmail.com>
2022-04-25 12:48:27 +0100

    new test utils

--- test/utils/network.ts
+++ test/utils/network.ts
@@ -1,5 +1,9 @@
 import { ethers } from "hardhat";
 
+export const SECONDS_PER_YEAR = 31536000;
+export const SECONDS_PER_MONTH = SECONDS_PER_YEAR / 12;
+export const SECONDS_PER_DAY = SECONDS_PER_MONTH / 30;
+
 export async function increaseTime(x: string | number): Promise<void> {
   await ethers.provider.send("evm_increaseTime", [x]);
   await ethers.provider.send("evm_mine", []);
--- test/utils/units.ts
+++ test/utils/units.ts
@@ -1,4 +1,8 @@
+import { BigNumber } from "ethers";
 import { ethers } from "hardhat";
+
+export const WAD = ethers.utils.parseUnits("1", 18);
+export const RAY = ethers.utils.parseUnits("1", 27);
 
 export const toWad = (amount: string | number) => {
   return ethers.utils.parseUnits(amount.toString(), "18");
@@ -14,4 +18,20 @@ export const toRay = (amount: string | number) => {
 
 export const fromRay = (amount: string | number) => {
   return ethers.utils.formatUnits(amount.toString(), "27");
+};
+
+export const wadToRay = (amount: string | number) => {
+  return ethers.utils.parseUnits(amount.toString(), "9");
+};
+
+export const rayToWad = (amount: string | number) => {
+  return ethers.utils.formatUnits(amount.toString(), "9");
+};
+
+export const wadMul = (a: BigNumber, b: BigNumber) => {
+  return a.mul(b).div(WAD);
+};
+
+export const rayMul = (a: BigNumber, b: BigNumber) => {
+  return a.mul(b).div(RAY);
 };
